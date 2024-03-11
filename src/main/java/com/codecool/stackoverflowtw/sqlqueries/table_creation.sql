32638fa07f6b855ff22536c999f56e91c3c08c5f
grindlevi <grindlevi@gmail.com>
2024-03-11 14:21:52 +0100

    table creation sql query added

---
+++ src/main/java/com/codecool/stackoverflowtw/sqlqueries/table_creation.sql
@@ -0,0 +1,6 @@
+CREATE TABLE questions (
+                           id INT PRIMARY KEY,
+                           title VARCHAR(255) NOT NULL,
+                           description TEXT,
+                           created TIMESTAMP
+);
